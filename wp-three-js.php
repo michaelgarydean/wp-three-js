16e5b9fe88b73dadbcb8b88b8bce5b201ef86e61
Mike Dean <contact@michaeldean.ca>
2020-11-23 12:59:21 +0000

    initial plugin files

---
+++ wp-three-js.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     WP Three.js
+ * Plugin URI:      https://github.com/mykedean/wp-three-js.git
+ * Description:     Load three.js scenes into canvas elements in a WordPress theme
+ * Author:          Michael GaryDean <contact@michaeldean.ca>
+ * Author URI:      https://github.com/mykedean/
+ * Text Domain:     wp-three-js
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Three_Js
+ */
